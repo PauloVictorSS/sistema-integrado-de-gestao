f381d383d080053656f0858e4b93db5b8a3709dc
Paulo Victor <p248438@dac.unicamp.br>
2022-10-22 09:43:49 -0300

    Adição da tela de login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,25 @@
+import { InputText } from "../components/InputText";
+import { Text } from "../components/Text"
+import {Envelope, Lock} from "phosphor-react"
 import { Button } from "../components/Button";
 
 function Login() {
+
   return (
-    <div className="bg-gray-900">
-      <Button text="Exemplo de botão"/>
+    <div className="w-screen h-screen bg-gray-900 flex gap-24 items-center justify-center text-gray-100">
+      <div className="flex flex-col gap-6 items-stretch w-full max-w-sm">
+        <label htmlFor="email" className="flex flex-col gap-3">
+          <Text className="font-semibold">Endereço de e-mail</Text>
+          <InputText icon={<Envelope/>} type="email" id="email" placeholder="Digite seu e-mail" />
+        </label>
+        <label htmlFor="password" className="flex flex-col gap-3">
+          <Text className="font-semibold">Senha</Text>
+          <InputText icon={<Lock/>} type="password" id="password" placeholder="********" />
+        </label>
+        <Button>Entrar no sistema</Button>
+      </div>
+      <div className="h-72 border-r-2"></div>
+      <h2 className="text-gray-100 font-bold text-3xl max-w-sm text-center">Sistema de Gestão Integrado</h2>
     </div>
   );
 }
