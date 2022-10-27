a6545b7a917fac034958dd48e716bc31e021862d
Paulo Victor <p248438@dac.unicamp.br>
2022-10-27 15:33:57 -0300

    Logica básica de alterar um componente

--- src/components/ComponentsCard.tsx
+++ src/components/ComponentsCard.tsx
@@ -5,6 +5,7 @@ import ModelBackground from "./ModelBackground";
 import {Text} from "../components/Text";
 import {InputText} from "../components/InputText";
 import { TextArea } from "./TextArea";
+import { useState } from "react";
 
 interface ComponentsCardProps{
     component: IComponent
@@ -13,6 +14,25 @@ interface ComponentsCardProps{
 }
 
 export function ComponentsCard({component, changeComponentsCard, isAddComponent}: ComponentsCardProps){
+
+    const [componentName, setComponentName] = useState(component.name)
+    const [componentQtd, setComponentQtd] = useState(component.qtd)
+    const [componentQtdMin, setComponentQtdMin] = useState(component.qtdMin)
+    const [componentLocal, setComponentLocal] = useState(component.local)
+    const [componentDescription, setComponentDescription] = useState(component.description)
+
+    const saveChanges = () => {
+
+        const currentComponent = {
+            id: component.id,
+            name: componentName,
+            qtd: componentQtd,
+            qtdMin: componentQtdMin,
+            local: componentLocal,
+            description: componentDescription,
+            lastUpdate: new Date().toLocaleString(),
+        } as IComponent
+    }
 
     return(
         <ModelBackground>
@@ -23,31 +43,64 @@ export function ComponentsCard({component, changeComponentsCard, isAddComponent}
                 <div className="flex flex-col gap-6 items-stretch w-full">
                     <label htmlFor="name" className="flex flex-col gap-3">
                         <Text className="font-semibold">Nome</Text>
-                        <InputText className="bg-gray-900" type="text" id="name" placeholder="nome do componente" />
+                        <InputText
+                            className="bg-gray-900"
+                            type="text"
+                            id="name"
+                            placeholder="nome do componente"
+                            value={componentName}
+                            onChange={(e) => setComponentName(e.target.value)}
+                        />
                     </label>
                     <label htmlFor="qtd" className="flex flex-col gap-3">
                         <Text className="font-semibold">Quantidade</Text>
-                        <InputText className="bg-gray-900" type="number" id="qtd" placeholder="quantidade de unidades do componente" />
+                        <InputText
+                            className="bg-gray-900"
+                            type="number"
+                            id="qtd"
+                            placeholder="quantidade de unidades do componente"
+                            value={componentQtd}
+                            onChange={(e) => setComponentQtd(parseInt(e.target.value))}
+                        />
                     </label>
                     <label htmlFor="qtdMin" className="flex flex-col gap-3">
                         <Text className="font-semibold">Quantidade mínima</Text>
-                        <InputText className="bg-gray-900" type="number" id="qtdMin" placeholder="quantidade mínima para o sistema avisar" />
+                        <InputText
+                            className="bg-gray-900"
+                            type="number"
+                            id="qtdMin"
+                            placeholder="quantidade mínima para o sistema avisar"
+                            value={componentQtdMin}
+                            onChange={(e) => setComponentQtdMin(parseInt(e.target.value))}
+                        />
                     </label>
                     <label htmlFor="local" className="flex flex-col gap-3">
                         <Text className="font-semibold">Local de armazenamento</Text>
-                        <InputText className="bg-gray-900" type="text" id="local" placeholder="gaveta onde está o componente" />
+                        <InputText
+                            className="bg-gray-900"
+                            type="text"
+                            id="local"
+                            placeholder="gaveta onde está o componente"
+                            value={componentLocal}
+                            onChange={(e) => setComponentLocal(e.target.value)}
+                        />
                     </label>
                     <label htmlFor="name" className="flex flex-col gap-3">
                         <Text className="font-semibold">Descrição</Text>
-                        <TextArea className="bg-gray-900"/>
+                        <TextArea
+                            className="bg-gray-900"
+                            placeholder="descrição do componente"
+                            value={componentDescription}
+                            onChange={(e) => setComponentDescription(e.target.value)}
+                        />
                     </label>
                     <div className="flex gap-2 w-full justify-between">
                         {isAddComponent ? 
-                            <Button onClick={changeComponentsCard}>
+                            <Button onClick={saveChanges}>
                                 Adicionar componente
                             </Button>
                             :
-                            <Button onClick={changeComponentsCard}>
+                            <Button onClick={saveChanges}>
                                 Confirmar alterações
                             </Button>
                         }
--- src/components/InputText.tsx
+++ src/components/InputText.tsx
@@ -8,7 +8,7 @@ interface InputTextProps extends InputHTMLAttributes<HTMLInputElement>{
 
 export function InputText({ icon, className, ...props }: InputTextProps) {
   return (
-    <div className={'flex items-center gap-3 h-12 p-2 rounded bg-gray-800 w-full max-w-sm outline-none focus-within:ring-2 ring-cyan-300 ' + className}>
+    <div className={'flex items-center gap-3 h-10 px-2 rounded bg-gray-800 w-full max-w-sm outline-none focus-within:ring-2 ring-cyan-300 ' + className}>
       <Slot className='w-6 h-6 text-gray-400'>
         {icon}
       </Slot>
