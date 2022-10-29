15af0f149eb6f220bd9ad7b3612b8648c01abc13
Paulo Victor <p248438@dac.unicamp.br>
2022-10-29 11:46:35 -0300

    Adição de um componente de carregamentp

---
+++ src/components/Loading.tsx
@@ -0,0 +1,12 @@
+import "../styles/loading.css"
+
+export function Loading() {
+  return (
+    <div className="lds-ring">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  )
+}
